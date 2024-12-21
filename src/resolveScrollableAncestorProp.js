fedde4ee63c19dfe23395e1d4cd195ca543cc3a3
treeFrog <zirhoo@gmail.com>
2024-12-21 09:42:08 -0800

    Update resolveScrollableAncestorProp.js

--- src/resolveScrollableAncestorProp.js
+++ src/resolveScrollableAncestorProp.js
@@ -2,7 +2,6 @@ export default function resolveScrollableAncestorProp(scrollableAncestor) {
   // When Waypoint is rendered on the server, `window` is not available.
   // To make Waypoint easier to work with, we allow this to be specified in
   // string form and safely convert to `window` here.
-  
   if (scrollableAncestor === 'window') {
     const globalWindow =
     typeof window !== 'undefined' ? window :
