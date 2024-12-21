c914b29e00c10a0fe75ee9e2ef1e1c1b5894ea36
treeFrog <zirhoo@gmail.com>
2024-12-21 09:39:26 -0800

    Missing global variable
    some builder such as vitejs don't have global variable in this context

--- src/resolveScrollableAncestorProp.js
+++ src/resolveScrollableAncestorProp.js
@@ -2,8 +2,13 @@ export default function resolveScrollableAncestorProp(scrollableAncestor) {
   // When Waypoint is rendered on the server, `window` is not available.
   // To make Waypoint easier to work with, we allow this to be specified in
   // string form and safely convert to `window` here.
+  const globalWindow =
+    typeof window !== 'undefined' ? window :
+    typeof global !== 'undefined' && global.window ? global.window :
+    undefined;
+
   if (scrollableAncestor === 'window') {
-    return global.window;
+    return globalWindow;
   }
 
   return scrollableAncestor;
